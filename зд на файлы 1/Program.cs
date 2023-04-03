3480582f3558b593e5791bb6b78e4cf78f981685
ri9ka <zshapcezuk@gmail.com>
2023-04-03 17:45:38 +0300

    Добавьте файлы проекта.

---
+++ зд на файлы 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        static void Main()
+        {
+            try
+            {
+                FileStream f = new FileStream("test.txt", FileMode.Create, FileAccess.ReadWrite); 
+                byte[] x = new byte[10];
+
+                for (byte i = 0; i < 10; ++i)
+                {
+                    x[i] = (byte)(10 + i);
+                }
+                f.Write(x, 0, 10);
+               
+                int count = 0;
+                f.Seek(0, SeekOrigin.Begin);
+                for (int i = 0; i < 10; i++)
+                {
+                   int a = f.ReadByte(); if (a % 2 == 0)
+                    {
+                        Console.Write(a + " ");
+                    }
+                    else
+                    {
+                        count += 1;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("нечетные числа -> " + count);
+                Console.WriteLine(); 
+                Console.WriteLine("текущая позиция ->" + f.Position);
+                f.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ошибка работы с файлом: " + e.Message);
+            }
+        }
+    }
+}
